9a2bc8fca5421e57a2b6b9287e3dea16393f689b
katehrybkova <katiushka152701@gmail.com>
2019-08-29 13:32:13 +0300

    last changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from './Header.module.css';
 import Menu from '../Menu/Menu';
 import icon from '../../assets/images/menu.svg';
 import menuLogo from '../../assets/images/menuLogo.png';
 
 
-const Header = () =>
-    <div className={style.header}>
-        <div>
-        <img width="25" src={icon} alt="icon" />
-        <img  src={menuLogo} alt="cow" />
-        <p>Mooogle</p>
-        </div>
-    </div>
+class Header extends Component {
+    state = {
+        menuOpen: false,
+    }
+    handleMenuOpen = () => {
+        const { menuOpen } = this.state;
+        this.setState({ menuOpen: !menuOpen });
+        console.log(this.state)
+    }
+    render() {
+        const { menuOpen } = this.state;
+        return (<div className={style.header}>
+            <div>
+                <img onClick={this.handleMenuOpen} width="25" src={icon} alt="icon" />
+                {menuOpen && < Menu  />}
+                <img src={menuLogo} alt="cow" />
+                <p>Mooogle</p>
+            </div>
+        </div>)
+    }
+}
 
 export default Header;
